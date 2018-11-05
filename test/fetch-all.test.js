e1b992a35f64c6269a2c625616c2c5f64882f7a4
Kevin Bieri <bierike@gmail.com>
2018-11-05 12:42:53 +0100

    Fix tests in fetch all

--- test/fetch-all.test.js
+++ test/fetch-all.test.js
@@ -16,51 +16,44 @@ class Dummy extends Model {
 
 test('Throws error when the client has no get method', () => {
   installPlugin();
-  expect(() => {
-    Model.fetchAll();
-  }).toThrow('HTTP Client has no `get` method');
+  expect(Model.fetchAll()).rejects.toEqual(new Error('HTTP Client has no `get` method'));
 });
 
 test('Throws error when entity name or apiPath is not defined', () => {
   const get = jest.fn();
   installPlugin({ get });
 
-  expect(() => {
-    EntityDummy.fetchAll();
-  }).toThrow("apiPath is not defined on class 'EntityDummy'");
-
-  expect(() => {
-    APIPathDummy.fetchAll();
-  }).toThrow("entity name is not defined on class 'APIPathDummy'");
+  expect(EntityDummy.fetchAll()).rejects.toEqual(new Error("apiPath is not defined on class 'EntityDummy'"));
+  expect(APIPathDummy.fetchAll()).rejects.toEqual(new Error("entity name is not defined on class 'APIPathDummy'"));
 });
 
-test('Calls the get method of the client when no constraint is violated', () => {
-  const get = jest.fn();
+test('Calls the get method of the client when no constraint is violated', async () => {
+  createStore(Dummy);
+  const get = jest.fn().mockReturnValue(mockResponse({}));
   installPlugin({ get });
 
-  Dummy.fetchAll();
+  await Dummy.fetchAll();
   expect(get).toHaveBeenCalledWith('dummyPath', { params: {} });
 });
 
 test('throws error when filter is not an object', () => {
-  expect(() => {
-    Dummy.fetchAll({ filter: '' });
-  }).toThrow('filter needs to be an object')
+  expect(Dummy.fetchAll({ filter: '' })).rejects.toEqual(new Error('filter needs to be an object'));
 });
 
-test('passes filter to get request', () => {
-  const get = jest.fn();
+test('passes filter to get request', async () => {
+  createStore(Dummy);
+  const get = jest.fn().mockReturnValue(mockResponse({}));
   installPlugin({ get });
 
-  Dummy.fetchAll({ filter: { search: '' } });
+  await Dummy.fetchAll({ filter: { search: '' } });
   expect(get).toHaveBeenCalledWith('dummyPath', { params: { search: '' } });
 });
 
 test('inserts fetched element in the database', async () => {
   const store = createStore(Dummy);
-  const get = jest.fn().mockReturnValue(mockResponse({ id: 1 }));
+  const get = jest.fn().mockReturnValue(mockResponse([{ id: 1 }]));
   installPlugin({ get });
   const response = await Dummy.fetchAll();
   expect(Dummy.find(1)).toEqual({ $id: 1 });
   expect(response).toEqual([{ $id: 1 }]);
-})
+});
