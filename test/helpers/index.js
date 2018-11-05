fca3ae3e005fa943bc711a91175210a046f1bae7
Kevin Bieri <bierike@gmail.com>
2018-11-05 11:27:13 +0100

    Implement store helpers

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,26 @@
 import install from '@/install';
 import * as components from '@vuex-orm/core';
+import Vuex from 'vuex';
+import Vue from 'vue';
+import VuexORM, { Database } from '@vuex-orm/core';
+
+Vue.use(Vuex);
 
 export function installPlugin(client = {}) {
   install(components, { client });
+}
+
+export function createStore(...entities) {
+  const database = new Database();
+
+  entities.forEach(entity => database.register(entity, {}));
+
+  return new Vuex.Store({
+    plugins: [VuexORM.install(database)],
+    strict: true,
+  });
+}
+
+export function mockResponse(data) {
+  return Promise.resolve({ data });
 }
