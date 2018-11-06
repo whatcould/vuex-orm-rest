4b93c54bc43047ecc814344314d21bb555375ae1
Kevin Bieri <bierike@gmail.com>
2018-11-06 15:43:06 +0100

    Check entity name before processing apiPath

--- src/api-path.js
+++ src/api-path.js
@@ -1,5 +1,8 @@
+import { checkEntityName } from '@/constraint';
 import joinPath from 'path.join';
 
 export default function apiPath() {
+  checkEntityName(this.constructor);
+
   return joinPath(this.constructor.apiPath, this.$id.toString());
 }
--- test/api-path.test.js
+++ test/api-path.test.js
@@ -6,10 +6,20 @@ class Dummy extends Model {
   static apiPath = 'dummyPath';
 }
 
+class EmptyDummy extends Model {}
+
+test('Throws error when no entity name is defined', () => {
+  installPlugin();
+
+  const emptyDummy = new EmptyDummy({ $id: 1 });
+
+  expect(() => { emptyDummy.apiPath(); }).toThrow("entity name is not defined on class 'EmptyDummy'");
+});
+
 test('Generates path with id', () => {
   installPlugin();
 
   const dummy = new Dummy({ $id: 1 });
 
-  expect(dummy.apiPath).toBe('dummyPath/1');
+  expect(dummy.apiPath()).toBe('dummyPath/1');
 });
