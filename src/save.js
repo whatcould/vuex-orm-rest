fc105ad5b0f320c5bd5fa7df79d75f792b598baa
David Reese <david@whatcould.com>
2021-05-19 13:45:35 -0400

    Save runs update unless object has a $uid

--- src/save.js
+++ src/save.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { checkConstraints } from '@/constraint';
 
 export default async function save(keys = Object.keys(this.$toJson())) {
-  if(this.id.indexOf('$uid') === -1) {
+  if(this.$id.indexOf('$uid') === -1) {
     return this.update(keys)
   }
   const { post } = this.client;
