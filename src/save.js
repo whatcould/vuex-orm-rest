22d5cc1c257f7161b522a7ab8a7839157987b381
David Reese <david@whatcould.com>
2021-05-20 07:26:57 -0400

    Add returned id to saved data & save that as new record
    Could see a preference for either way, but atm I would prefer not to return the entire record

--- src/save.js
+++ src/save.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
 import { checkConstraints } from '@/constraint';
 
+// saving a null relation causes an error in vuex-orm
+function cleanNulls(obj) {
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName];
+    }
+  }
+  return obj
+}
+
 export default async function save(keys = Object.keys(this.$toJson())) {
-  if(this.$id.indexOf('$uid') === -1) {
+  if(this.$id && this.$id.indexOf('$uid') === -1) {
     return this.update(keys)
   }
+
   const { post } = this.client;
 
   if (_.isUndefined(post)) {
@@ -13,7 +24,8 @@ export default async function save(keys = Object.keys(this.$toJson())) {
 
   checkConstraints(this);
 
-  const data = await post(this.constructor.apiPath, this.pickKeys(keys));
-  const stored = await this.$insert(data);
+  const data = await post(this.constructor.apiPath, this.nestParams(this.pickKeys(keys)));
+  let updateWithId = Object.assign(this.$toJson(), {id: data.data.id})
+  const stored = await this.$insert({data: cleanNulls(updateWithId)});
   return stored[this.constructor.entity][0];
 }
